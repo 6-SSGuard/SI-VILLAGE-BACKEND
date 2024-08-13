50aa770d23cc80f534b375e61a601bc48c078ce3
lazarus0320 <astar5327z@gmail.com>
2024-08-13 10:08:02 +0900

    Feat: first commit

---
+++ src/main/java/org/example/sivillage/SiVillageApplication.java
@@ -0,0 +1,13 @@
+package org.example.sivillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiVillageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiVillageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sivillage/SiVillageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sivillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiVillageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
