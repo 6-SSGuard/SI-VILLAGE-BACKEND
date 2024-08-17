48de47f0340f7a321e36ddc27b5378385f68c5f9
lazarus0320 <astar5327z@gmail.com>
2024-08-17 16:09:35 +0900

    Test: DB 동작 테스트

--- src/main/java/org/example/sivillage/member/domain/Member.java
+++ src/main/java/org/example/sivillage/member/domain/Member.java
@@ -1,4 +1,17 @@
 package org.example.sivillage.member.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
