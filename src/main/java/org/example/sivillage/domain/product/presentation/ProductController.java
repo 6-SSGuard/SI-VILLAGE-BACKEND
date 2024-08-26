fa7ecc68534c21296b29d8325d8b0a7c2f7ba5c8
lazarus0320 <astar5327z@gmail.com>
2024-08-26 16:27:12 +0900

    Fix: controller return 부분 변경

--- src/main/java/org/example/sivillage/domain/product/presentation/ProductController.java
+++ src/main/java/org/example/sivillage/domain/product/presentation/ProductController.java
@@ -11,7 +11,7 @@ import org.example.sivillage.domain.product.vo.CreateProductRequest;
 import org.example.sivillage.domain.product.vo.LikeProductRequest;
 import org.example.sivillage.domain.product.vo.UnLikeProductRequest;
 import org.example.sivillage.global.common.CustomResponseEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "물품 관리 API", description = "물품 관련 API endpoints")
@@ -27,31 +27,31 @@ public class ProductController {
     
     """)
     @PostMapping("/")
-    public ResponseEntity<CustomResponseEntity<?>> createProduct(@Valid @RequestBody CreateProductRequest request) {
+    public CustomResponseEntity<?> createProduct(@Valid @RequestBody CreateProductRequest request) {
         productService.createProduct(request);
-        return ResponseEntity.ok(new CustomResponseEntity<>("물품 생성이 완료되었습니다."));
+        return new CustomResponseEntity<>(HttpStatus.OK, "물품 생성이 완료되었습니다.");
     }
 
     @Operation(summary = "물품 상세 정보 조회", description = """
     
     """)
     @GetMapping("/details/{productId}")
-    public ResponseEntity<CustomResponseEntity<?>> getProductDetails(@PathVariable Long productId) {
-        return ResponseEntity.ok(new CustomResponseEntity<>(productService.getProductDetails(productId),
-                "물품 상세정보 조회가 완료되었습니다."));
+    public CustomResponseEntity<?> getProductDetails(@PathVariable Long productId) {
+        return new CustomResponseEntity<>(HttpStatus.OK, productService.getProductDetails(productId),
+                "물품 상세정보 조회가 완료되었습니다.");
     }
 
     @Operation(summary = "좋아요 추가", description = "특정 상품에 대해 좋아요를 추가합니다.")
     @PostMapping("/like")
-    public ResponseEntity<CustomResponseEntity<?>> likeProduct(@RequestBody LikeProductRequest request) {
+    public CustomResponseEntity<?> likeProduct(@RequestBody LikeProductRequest request) {
         productLikeService.likeProduct(request);
-        return ResponseEntity.ok(new CustomResponseEntity<>("좋아요가 추가되었습니다."));
+        return new CustomResponseEntity<>(HttpStatus.OK, "좋아요가 추가되었습니다.");
     }
 
     @Operation(summary = "좋아요 취소", description = "특정 상품에 대해 좋아요를 취소합니다.")
     @DeleteMapping("/like")
-    public ResponseEntity<CustomResponseEntity<?>> unlikeProduct(@RequestBody UnLikeProductRequest request) {
+    public CustomResponseEntity<?> unlikeProduct(@RequestBody UnLikeProductRequest request) {
         productLikeService.unlikeProduct(request);
-        return ResponseEntity.ok(new CustomResponseEntity<>("좋아요가 취소되었습니다."));
+        return new CustomResponseEntity<>(HttpStatus.OK, "좋아요가 취소되었습니다.");
     }
 }
