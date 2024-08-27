2ba610b40762852d6714c2561c930740a8e3a6fe
lazarus0320 <astar5327z@gmail.com>
2024-08-27 16:54:07 +0900

    Confilct: BaseResponse 관련 충돌 해결

--- src/main/java/org/example/sivillage/auth/presentation/AuthController.java
+++ src/main/java/org/example/sivillage/auth/presentation/AuthController.java
@@ -8,18 +8,18 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.sivillage.auth.domain.CustomUserDetails;
-import org.example.sivillage.global.common.CustomResponseEntity;
-import org.example.sivillage.global.util.JwtToken;
-import org.example.sivillage.global.util.JwtTokenProvider;
-import org.example.sivillage.member.application.MemberService;
 import org.example.sivillage.auth.vo.LogInRequest;
 import org.example.sivillage.auth.vo.RefreshTokenRequest;
 import org.example.sivillage.auth.vo.SignUpRequest;
-import org.springframework.http.HttpStatus;
+import org.example.sivillage.global.common.response.BaseResponse;
+import org.example.sivillage.global.util.JwtToken;
+import org.example.sivillage.global.util.JwtTokenProvider;
+import org.example.sivillage.member.application.MemberService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "계정 관리 API", description = "계정 관련 API endpoints")
 @RestController
@@ -43,9 +43,9 @@ public class AuthController {
             @ApiResponse(responseCode = "409", description = "이메일 중복")
     })
     @PostMapping("/signup")
-    public CustomResponseEntity<?> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
+    public BaseResponse<Void> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
         memberService.signUp(signUpRequest);
-        return new CustomResponseEntity<>(HttpStatus.OK, "회원가입이 완료되었습니다!");
+        return new BaseResponse<>();
     }
 
     @Operation(summary = "로그인", description = """
@@ -57,7 +57,7 @@ public class AuthController {
             @ApiResponse(responseCode = "404", description = "사용자 없음")
     })
     @PostMapping("/login")
-    public CustomResponseEntity<JwtToken> logIn(@Valid @RequestBody LogInRequest request) {
+    public BaseResponse<JwtToken> logIn(@Valid @RequestBody LogInRequest request) {
         String code = request.getEmail();
         String password = request.getPassword();
 
@@ -66,15 +66,15 @@ public class AuthController {
         log.debug("request code = {}, password = {}", code, password);
         log.debug("jwtToken accessToken = {}, refreshToken = {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
 
-        return new CustomResponseEntity<>(HttpStatus.OK, jwtToken, "로그인 성공.");
+        return new BaseResponse<>(jwtToken);
     }
 
 
     @Operation(summary = "Access Token 재발급", description = "Access Token 만료시 기존에 발급받은 Refresh Token을 이쪽으로 보내서 새로운 Access Token 받아가기")
     @PostMapping("/refresh")
-    public CustomResponseEntity<?> refreshAccessToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+    public BaseResponse<JwtToken> refreshAccessToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
         JwtToken token = jwtTokenProvider.refreshAccessToken(refreshTokenRequest.getRefreshToken());
-        return new CustomResponseEntity<>(HttpStatus.OK, token, "Access Token 재발급 완료.");
+        return new BaseResponse<>(token);
     }
 
     @Operation(summary = "로그아웃", description = "현재 로그인 된 계정의 로그아웃")
@@ -83,19 +83,11 @@ public class AuthController {
             @ApiResponse(responseCode = "401", description = "인증 실패")
     })
     @PostMapping("/logout")
-    public CustomResponseEntity<?> logout(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public BaseResponse<Void> logout(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         String email = customUserDetails.getUsername();
         jwtTokenProvider.deleteRefreshToken(email);
-        return new CustomResponseEntity<>(HttpStatus.OK, "로그아웃 되었습니다.");
+        return new BaseResponse<>();
     }
-
-    @Operation(summary = "swagger test", description = "swagger 테스트를 위한 엔드포인트 입니다.")
-    @GetMapping("/test")
-    public CustomResponseEntity<?> test() {
-        List<Integer> testList = List.of(1, 2, 3, 4, 5); //테스트
-        return new CustomResponseEntity<>(HttpStatus.OK, testList, "테스트 리스트 반환 완료");
-    }
-
 }
 
 
--- src/main/java/org/example/sivillage/member/presentation/MemberController.java
+++ src/main/java/org/example/sivillage/member/presentation/MemberController.java
@@ -6,11 +6,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.sivillage.auth.domain.CustomUserDetails;
-import org.example.sivillage.global.common.CustomResponseEntity;
+import org.example.sivillage.global.common.response.BaseResponse;
 import org.example.sivillage.member.application.BeautyInfoService;
 import org.example.sivillage.member.dto.in.BeautyInfoRequestDto;
 import org.example.sivillage.member.dto.out.BeautyInfoResponseDto;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,26 +24,26 @@ public class MemberController {
 
     @Operation(summary = "뷰티 정보 등록", description = "뷰티정보를 등록합니다.")
     @PostMapping("/beauty-info")
-    public CustomResponseEntity<Void> addBeautyInfo(@Valid @RequestBody BeautyInfoRequestDto dto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public BaseResponse<Void> addBeautyInfo(@Valid @RequestBody BeautyInfoRequestDto dto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         String memberUuid = "uuid";
         beautyInfoService.addBeautyInfo(dto, memberUuid);
-        return new CustomResponseEntity<>(HttpStatus.OK, "뷰티정보 등록을 완료하였습니다.");
+        return new BaseResponse<>();
     }
 
     @Operation(summary = "뷰티 정보 조회", description = "뷰티정보를 조회합니다.")
     @GetMapping("/beauty-info")
-    public CustomResponseEntity<BeautyInfoResponseDto> getBeautyInfo(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public BaseResponse<BeautyInfoResponseDto> getBeautyInfo(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         String memberUuid = "uuid";
         BeautyInfoResponseDto responseDto = beautyInfoService.getBeautyInfo(memberUuid);
-        return new CustomResponseEntity<>(HttpStatus.OK, responseDto, "뷰티정보 조회를 완료하였습니다.");
+        return new BaseResponse<>(responseDto);
     }
 
     @Operation(summary = "뷰티 정보 수정", description = "뷰티정보를 수정합니다.")
     @PutMapping("/beauty-info")
-    public CustomResponseEntity<Void> changeBeautyInfo(@Valid @RequestBody BeautyInfoRequestDto dto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public BaseResponse<Void> changeBeautyInfo(@Valid @RequestBody BeautyInfoRequestDto dto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         String memberUuid = "uuid";
         beautyInfoService.changeBeautyInfo(dto, memberUuid);
-        return new CustomResponseEntity<>(HttpStatus.OK, "뷰티정보 수정을 완료하였습니다.");
+        return new BaseResponse<>();
     }
 }
 
