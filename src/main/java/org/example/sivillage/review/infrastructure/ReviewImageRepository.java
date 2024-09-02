5b46ec1588d7d17d49bd1839f2147474b36246c7
kang-subin <tnbin98@gmail.com>
2024-09-02 17:55:40 +0900

    Fix: ReviewImageRepository 수정
    List 형태로 수정

--- src/main/java/org/example/sivillage/review/infrastructure/ReviewImageRepository.java
+++ src/main/java/org/example/sivillage/review/infrastructure/ReviewImageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
-    List<ReviewImage> findByReview(Review review);
+    List <ReviewImage> findByReview(Review review);
     void deleteAllByReview(Review review);
 }
