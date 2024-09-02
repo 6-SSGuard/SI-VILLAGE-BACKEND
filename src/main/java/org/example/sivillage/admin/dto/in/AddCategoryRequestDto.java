6fcebf7713220c72bae1775f3567c2ba662b78b6
lazarus0320 <astar5327z@gmail.com>
2024-09-03 00:13:19 +0900

    Feat: json 파일 업로드로 카테고리 생성하도록 구현

--- src/main/java/org/example/sivillage/admin/dto/in/AddCategoryRequestDto.java
+++ src/main/java/org/example/sivillage/admin/dto/in/AddCategoryRequestDto.java
@@ -1,12 +1,15 @@
 package org.example.sivillage.admin.dto.in;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddCategoryRequestDto {
     private String categoryName;
-    private String categoryDescription;
     private String parentCategoryCode;
 }
--- src/main/java/org/example/sivillage/admin/presentation/CategoryController.java
+++ src/main/java/org/example/sivillage/admin/presentation/CategoryController.java
@@ -3,13 +3,12 @@ package org.example.sivillage.admin.presentation;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.example.sivillage.admin.application.CategoryService;
-import org.example.sivillage.admin.dto.in.AddCategoryRequestDto;
 import org.example.sivillage.admin.dto.out.GetSubCategoriesResponseDto;
-import org.example.sivillage.admin.vo.AddCategoryRequestVo;
 import org.example.sivillage.admin.vo.GetSubCategoriesResponseVo;
 import org.example.sivillage.global.common.response.BaseResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,12 +18,19 @@ public class CategoryController {
     private final CategoryService categoryService;
     private final ModelMapper mapper;
 
-    @Operation(summary = "카테고리 생성", description = "parentCategoryCode =\"top\"입력시 최상위 카테고리 생성")
-    @PostMapping("/")
-    public BaseResponse<Void> addCategory(
-            @RequestBody AddCategoryRequestVo addCategoryRequestVo) {
-        AddCategoryRequestDto request = mapper.map(addCategoryRequestVo, AddCategoryRequestDto.class);
-        categoryService.addCategory(request);
+//    @Operation(summary = "카테고리 생성", description = "parentCategoryCode =\"top\"입력시 최상위 카테고리 생성")
+//    @PostMapping("/")
+//    public BaseResponse<Void> addCategory(
+//            @RequestBody AddCategoryRequestVo addCategoryRequestVo) {
+//        AddCategoryRequestDto request = mapper.map(addCategoryRequestVo, AddCategoryRequestDto.class);
+//        categoryService.addCategory(request);
+//        return new BaseResponse<>();
+//    }
+
+    @Operation(summary = "JSON 파일 기반으로 카테고리 생성")
+    @PostMapping(value = "/json", consumes = "multipart/form-data")
+    public BaseResponse<Void> addCategoryFromFile(@RequestPart("file") MultipartFile file) {
+        categoryService.addCategoryFromFile(file);
         return new BaseResponse<>();
     }
 
