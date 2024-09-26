4ea6e50af41da4e33921a9f96bcc1b5908603b37
lazarus0320 <astar5327z@gmail.com>
2024-09-26 22:32:11 +0900

    Feat: 상품 데이터 10만개, 리뷰 데이터 추가

--- src/main/java/org/example/sivillage/review/application/ReviewImageServiceImpl.java
+++ src/main/java/org/example/sivillage/review/application/ReviewImageServiceImpl.java
@@ -7,7 +7,6 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.example.sivillage.global.common.response.BaseResponseStatus;
 import org.example.sivillage.global.error.BaseException;
-import org.example.sivillage.review.domain.Review;
 import org.example.sivillage.review.domain.ReviewImage;
 import org.example.sivillage.review.dto.in.ReviewImageRequestDto;
 import org.example.sivillage.review.dto.out.ReviewImageResponseDto;
@@ -19,6 +18,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Random;
 
 @Service
 @Transactional
@@ -26,6 +26,8 @@ import java.util.List;
 public class ReviewImageServiceImpl implements ReviewImageService{
 
     private final ReviewImageRepository reviewImageRepository;
+
+
 
     // 등록
     public void addReviewImage(List<ReviewImageRequestDto> reviewImageRequestDto, Long reviewId) {
@@ -42,19 +44,26 @@ public class ReviewImageServiceImpl implements ReviewImageService{
              CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
 
             List<CSVRecord> records = csvParser.getRecords();
+            Random random = new Random();
+
             for (CSVRecord record : records) {
+                // 1~100 사이의 랜덤 숫자를 생성
+                int randomImageCount = random.nextInt(100) + 1;
+
                 Long id = Long.valueOf(record.get("id")); // CSV 컬럼 이름 사용
                 String reviewImageUrl = record.get("reviewImageUrl");
                 Long reviewId = Long.valueOf(record.get("reviewId"));
 
-                // 리뷰이미지 엔티티 생성 및 저장
-                ReviewImage reviewImage = ReviewImage.builder()
-                        .id(id)
-                        .reviewImageUrl(reviewImageUrl)
-                        .reviewId(reviewId)
-                        .build();
+                // 랜덤한 수만큼 리뷰 이미지 생성 및 저장
+                for (int i = 0; i < randomImageCount; i++) {
+                    ReviewImage reviewImage = ReviewImage.builder()
+                            .id(id)  // id는 고유해야 하므로 로직상 다르게 처리해야 할 수 있음 (랜덤 생성 또는 시퀀스 사용)
+                            .reviewImageUrl(reviewImageUrl)
+                            .reviewId(reviewId)
+                            .build();
 
-             reviewImageRepository.save(reviewImage);
+                    reviewImageRepository.save(reviewImage);
+                }
             }
         } catch (Exception e) {
             throw new BaseException(BaseResponseStatus.INTERNAL_SERVER_ERROR);
